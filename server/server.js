92beb110c2efe06d13892fe4f14511d0781d1ae1
Candy Kuo <candykuo@Candys-MacBook-Pro.local>
2017-07-06 14:22:03 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -27,8 +28,25 @@ app.get('/todos', (req, res) => {
     res.send({todos});
 
   }, (e) => {
-    res.status(400).send(e);
+    res.status(400).send();
   })
+})
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    return res.send({todo});
+
+  }).catch((e) => res.status(400).send());
 })
 
 app.listen(3000, () => {
